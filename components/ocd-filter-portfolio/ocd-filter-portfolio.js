b0023c30951fbfdcfc06dd081f7a143dcba13567
Jeremy Kozan <jeremyakozan@gmail.com>
2024-11-21 01:10:00 -0300

    reverted previous change... moving modals on click was a bad idea. just do it on page load.

--- components/ocd-filter-portfolio/ocd-filter-portfolio.js
+++ components/ocd-filter-portfolio/ocd-filter-portfolio.js
@@ -184,11 +184,7 @@ jQuery(function ($) {
     /***************************** END ISOTOPE *******************************/
 
     /***************************** MODALS *******************************/
-    // $(".ocdfp-wrapper .ocdfp-modal").appendTo("body");
-    // dont do it on page load... wait until click to move modal div to body container... idk if this is better? (may cause problems)
-    $(".ocdfp-wrapper .ocdfp-modal").on("click", function () {
-      $(this).appendTo("body");
-    });
+    $(".ocdfp-wrapper .ocdfp-modal").appendTo("body");
 
     window.ocdHtmlDocStyleAttrStr = "";
 
